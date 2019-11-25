a03460a35ec686e6999bbae611b7f63e2d3e1321
ardakkk <ardakfuse@gmail.com>
2019-11-25 12:41:04 +0600

    Behavioral | Implemented Observer pattern

--- behavioral/Observer/Example.js
+++ behavioral/Observer/Example.js
@@ -1,11 +1,11 @@
-const Subject = () => {
+var Subject = function() {
     this.observers = [];
     
     return {
         subscribeObserver: (observer) => {
             this.observers.push(observer);
         },
-        unsubscribeObserver: () => {
+        unsubscribeObserver: (observer) => {
             const index = this.observers.indexOf(observer);
             
             if (index > -1) {
@@ -16,18 +16,18 @@ const Subject = () => {
             const index = this.observers.indexOf(observer);
             
             if (index > -1) {
-                this.observers[index].notify(index);
+                this.observers[index].notify(index + 1);
             }
         },
         notifyAllObservers: () => {
             for (let i = 0; i < this.observers.length; i++) {
-                this.observers[i].notify(i);
+                this.observers[i].notify(i + 1);
             }
         }
     }
 }
 
-const Observer = () => {
+const Observer = function () {
     return {
         notify: (index) => {
             console.log(`Observer ${index} is notified!`);
@@ -41,6 +41,11 @@ const observer1 = new Observer();
 const observer2 = new Observer();
 const observer3 = new Observer();
 const observer4 = new Observer();
+
+subject.subscribeObserver(observer1);
+subject.subscribeObserver(observer2);
+subject.subscribeObserver(observer3);
+subject.subscribeObserver(observer4);
 
 subject.notifyObserver(observer2); // Observer 2 is notified!
 
